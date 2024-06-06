7f3a42774b7ebcf4d281eb16e45666426347d53c
Moacir Pereira da Silva <moacirsilva.it@gmail.com>
2024-06-06 15:20:02 -0300

    create entities

---
+++ UserMangerAPI/Domain/Entities/GrupoUsuario.cs
@@ -0,0 +1,12 @@
+﻿namespace UserManagerAPI.Domain.Entities
+{
+    public class GrupoUsuario
+    {
+        public int IdGrupo { get; set; }
+        public string? Descricao { get; set; }
+        public bool Administrador { get; set; }
+        public DateTime? DataCadastro { get; set; }
+        public DateTime? DataAlteracao { get; set; }
+
+    }
+}
---
+++ UserMangerAPI/Domain/Entities/Usuario.cs
@@ -0,0 +1,13 @@
+﻿namespace UserManagerAPI.Domain.Entities
+{
+    public class Usuario
+    {
+        public int IdUsuario { get; set; }
+        public required string Nome {  get; set; }
+        public required string Senha { get; set; }
+        public required string CPF { get; set; }
+        public int IdGrupo { get; set; }
+        public DateTime? DataCadastro { get; set; }
+        public DateTime? DataAlteracao { get; set; }
+    }
+}
